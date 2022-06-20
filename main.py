b476f7ef4264c6c432be2f9f58cc91d2f1c97d55
Sekhar <aksekhar.ai@gmail.com>
2022-06-20 11:42:51 +0530

    commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ from common.get_spark_session import GetSparkSession
 
 if __name__ == '__main__':
     print('load params')
-
     spark = GetSparkSession.get_spark_session('local', 'db_operation')
     print(spark)
 
